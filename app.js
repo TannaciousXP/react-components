ce3baaa5bffca130016cac30c9a79ed0f6893ead
Peter Tan <pxhtan@gmail.com>
2017-06-02 14:18:03 -0700

    Working up to class components

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // TODO
+/*
+var GroceryList = (GroceryListItem) => (
+  <ul>
+    <li>{GroceryListItem.toget[0]}</li>
+    <li>{GroceryListItem.toget[1]}</li>
+    <li>{GroceryListItem.toget[2]}</li>   
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>GroceryList Items</h2>
+    <GroceryList toget={['apples', 'eggs', 'veggies']}/>
+  </div>
+);
+*/
+
+
+/*
+// props in this instance is GroceryListItem and they are immutable
+var GroceryList = (GroceryListItem) => {
+  
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{GroceryListItem.toget[0]}</li>
+      <li>{GroceryListItem.toget[1]}</li>
+      <li>{GroceryListItem.toget[2]}</li>   
+    </ul>
+  );
+};
+
+var App = () => (
+  <div>
+    <h2>GroceryList Items</h2>
+    <GroceryList toget={['apples', 'eggs', 'veggies']}/>
+  </div>
+);
+
+
+
+ReactDOM.render(<App />, document.getElementById ('app'));
+*/
